2fc850fdc6eed3104532edc720609bd0e9ec85ab
mreed6 <reed3@oakland.edu>
2021-04-03 16:33:39 -0400

    Test03

--- storage.php
+++ storage.php
@@ -3,47 +3,16 @@
     <title>PHP Test</title>
 </head>
 <body>
-<?PHP
-$dir = "/home/msreed1811/csi3670/";
-function getFileList($dir)
-{
-    // array to hold return value
-    $retval = [];
+<?php
+$dir    = '/home/msreed1811/csi3670';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
 
-    // add trailing slash if missing
-    if(substr($dir, -1) != "/") {
-        $dir .= "/";
-    }
-
-    // open pointer to directory and read list of files
-    $d = @dir($dir) or die("getFileList: Failed opening directory {$dir} for reading");
-    while(FALSE !== ($entry = $d->read())) {
-        // skip hidden files
-        if($entry{0} == ".") continue;
-        if(is_dir("{$dir}{$entry}")) {
-            $retval[] = [
-                'name' => "{$dir}{$entry}/",
-                'type' => filetype("{$dir}{$entry}"),
-                'size' => 0,
-                'lastmod' => filemtime("{$dir}{$entry}")
-            ];
-        } elseif(is_readable("{$dir}{$entry}")) {
-            $retval[] = [
-                'name' => "{$dir}{$entry}",
-                'type' => mime_content_type("{$dir}{$entry}"),
-                'size' => filesize("{$dir}{$entry}"),
-                'lastmod' => filemtime("{$dir}{$entry}")
-            ];
-        }
-    }
-    $d->close();
-
-    return $retval;
-    echo $dir;
-}
+print_r($files1);
+print_r($files2);
 ?>
 
-    <p>This is my dir: <?php$dir = "/home/msreed1811/csi3670/";?></p>
+<!--    <p>This is my dir: --><?php$//dir = "/home/msreed1811/csi3670/";?><!--</p>-->
 </body>
 </html>
 
