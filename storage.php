f5a944c208f2b89fa7143e9de2b79197a3a3e8cf
mreed6 <reed3@oakland.edu>
2021-04-03 17:06:21 -0400

    Test08

--- storage.php
+++ storage.php
@@ -13,7 +13,23 @@
         while($file = readdir($except)){
             echo $file . '<br>';
         }
+
+        if(isset($_POST['submit'])){
+            $text = $_POST['text'];
+            $file = fopen("test04.txt", "w+") or die("file not open");
+
+            $inject = $text. "\n";
+
+            fputs($file, $inject) or die("Data not written to file");
+
+            fclose($file);
+        }
         ?>
+
+        <form action="#" method="post">
+            Write to file:<input type="text" name="text" ><br>
+            <input type="submit" name="submit" value="Write to file" ><br>
+        </form>
     </body>
 </html>
 
