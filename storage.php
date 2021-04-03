629c24376de0f817de37da84af31874069ce32aa
mreed6 <reed3@oakland.edu>
2021-04-03 16:48:23 -0400

    Test07

--- storage.php
+++ storage.php
@@ -1,19 +1,20 @@
 <html>
-<head>
-    <title>PHP Test</title>
-</head>
-<body>
-<?php
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php
+        $dir = '/storage';
 
-$dir = '/storage';
-$files = scandir($dir);
-print_r($files);
+        if($except = opendir($dir.'/')){
+            echo "Looking inside the '$dir'" . '<br>';
+        }
 
-echo $files;
-?>
-
-<!--    <p>This is my dir: --><?php$//dir = "/home/msreed1811/csi3670/";?><!--</p>-->
-</body>
+        while($file = readdir($except)){
+            echo $file . '<br>';
+        }
+        ?>
+    </body>
 </html>
 
 
